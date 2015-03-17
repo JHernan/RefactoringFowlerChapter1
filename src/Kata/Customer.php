9eec829639b234e203da7b44864ab71a043ed09f
Jorge Hernán <jorge.hernan.esparza@gmail.com>
2015-03-17 23:56:10 +0100

    Move Method - Move method amountFor from Customer Class to Rental Class

--- src/Kata/Customer.php
+++ src/Kata/Customer.php
@@ -58,23 +58,7 @@ class Customer
      */
     private function amountFor($rental)
     {
-        $result = 0;
-        switch ($rental->getMovie()->getPriceCode()) {
-            case Movie::REGULAR:
-                $result += 2;
-                if ($rental->getDaysRented() > 2)
-                    $result += ($rental->getDaysRented() - 2) * 1.5;
-                break;
-            case Movie::NEW_RELEASE:
-                $result += $rental->getDaysRented() * 3;
-                break;
-            case Movie::CHILDREN:
-                $result += 1.5;
-                if ($rental->getDaysRented() > 3)
-                    $result += ($rental->getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return $result;
+        return $rental->getCharge();
     }
 }
 
